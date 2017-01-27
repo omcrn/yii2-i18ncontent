7028b14aa1d108a5332e2f25e677bc4deec74da6
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2017-01-27 13:09:30 +0400

    Add deprication to BaseHelper::getAvailableLocales method

--- helpers/BaseHelper.php
+++ helpers/BaseHelper.php
@@ -6,11 +6,18 @@ use Yii;
 /**
  * Class BaseHelper
  *
- * @author zura
+ * @author Zura Sekhniashvili <zurasekhniashvili@gmail.com>
  * @package centigen\i18ncontent\helpers
  */
 class BaseHelper 
 {
+    /**
+     * Returns availableLocales from `Yii::$app->params` if it is set. If not it will return Yii::$app->language
+     *
+     * @author Zura Sekhniashvili <zurasekhniashvili@gmail.com>
+     * @return array
+     * @deprecated Will be removed on v2.0.0. Use LocaleHelper::getAvailableLocales instead
+     */
     public static function getAvailableLocales()
     {
         $locales = [];
