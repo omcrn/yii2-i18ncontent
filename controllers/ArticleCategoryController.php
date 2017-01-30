9124471fcdc72755ff9611ed9eef1d6c2b3c0989
koco <keshikashviligio@gmail.com>
2017-01-30 11:39:09 +0400

    fixed type in comment

--- controllers/ArticleCategoryController.php
+++ controllers/ArticleCategoryController.php
@@ -7,6 +7,7 @@ use centigen\i18ncontent\models\ArticleCategory;
 use centigen\i18ncontent\models\search\ArticleCategorySearch;
 use centigen\i18ncontent\web\Controller;
 use Yii;
+use yii\helpers\Url;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
@@ -122,7 +123,12 @@ class ArticleCategoryController extends Controller
      */
     public function actionDelete($id)
     {
-        $this->findModel($id)->delete();
+        $model = $this->findModel($id);
+        if(count($model->articleCategoryArticles) > 0){
+            $msg = Yii::t('i18ncontent', 'This category has articles. In order to delete this category, first you must delete all articles in this category {link}', ['link' => '<a href="'.Url::to(['article/index', 'ArticleSearch' => ['category_ids' => $model->id]]).'">'.Yii::t('i18ncontent', 'View them').'</a>']);
+            return $this->renderContent($msg);
+        }
+        $model->delete();
 
         return $this->redirect(['index']);
     }
